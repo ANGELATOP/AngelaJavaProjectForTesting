58f65a4b5c93f5d567f9a0910147cd384062bcd1
atopp <atopp@AngelaAcerHome>
2026-06-11 13:48:19 -0500

    checkin

---
+++ AngelaJavaProjectForTesting/src/Utility/Tester.java
@@ -0,0 +1,20 @@
+package Utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> test = new ArrayList<String>();
+		
+		test.add("a");
+		test.add("a");
+
+		List<String> test2;
+		System.out.println(test.get(3));
+	}
+
+}
